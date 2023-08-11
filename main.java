7dba4f5b12775c995db5b9672fad295eb3520039
Sümeyye <58435258+svcsmy@users.noreply.github.com>
2023-08-11 17:10:38 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+
+	for(int i = 1; i <= 10; i++) {
+		System.out.println("*************************");
+		  for(int j = 1; j <= 10; j++) {
+			
+			  
+		System.out.println(i + "x" + j + "=" + (i*j));
+		
+		  }
+		  System.out.println("*************************");	
+		
+		
+		
+		
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
